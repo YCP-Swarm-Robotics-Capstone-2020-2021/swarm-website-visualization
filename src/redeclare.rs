44de070f9fcd769ac022f23b99b96c677eb42416
Earl Kennedy <mnenmenth@gmail.com>
2020-08-18 20:35:04 -0400

    Some macros to reduce boilerplate of things such as `let x = x.clone(); let y = y.clone();`, etc.

---
+++ src/redeclare.rs
@@ -0,0 +1,66 @@
+//! Various macros to redeclare a variable with a new value
+//! Used to reduce boilerplate code of `let x = x.clone(); let y = y.clone();`, etc.
+
+macro_rules! clone
+{
+    ($($var:ident),+) =>
+    {
+        $(
+            let $var = $var.clone();
+        )+
+    };
+    ($($parent:ident.$var:ident),+) =>
+    {
+        $(
+            let $var = $parent.$var.clone();
+        )+
+    };
+}
+
+macro_rules! wrap
+{
+    ($($var:ident),+) =>
+    {
+        $(
+            let $var = Rc::new(RefCell::new($var));
+        )+
+    };
+    ($($parent:ident.$var:ident),+) =>
+    {
+        $(
+            let $var = Rc::new(RefCell:new($parent.$var));
+        )+
+    }
+}
+
+macro_rules! borrow
+{
+    ($($var:ident),+) =>
+    {
+        $(
+            let $var = $var.borrow();
+        )+
+    };
+    ($($parent:ident.$var:ident),+) =>
+    {
+        $(
+            let $var = $parent.$var.borrow();
+        )+
+    };
+}
+
+macro_rules! borrow_mut
+{
+    ($($var:ident),+) =>
+    {
+        $(
+            let mut $var = $var.borrow_mut();
+        )+
+    };
+    ($($parent:ident.$var:ident),+) =>
+    {
+        $(
+            let $var = $parent.$var.borrow_mut();
+        )+
+    };
+}
