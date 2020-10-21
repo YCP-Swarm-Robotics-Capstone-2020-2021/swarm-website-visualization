3ad0f0bd732a6c3270e7fa548eea705236457074
Earl Kennedy <mnenmenth@gmail.com>
2020-10-21 13:18:58 -0400

    Add function skeletons to resource loader

--- src/resource/loader.rs
+++ src/resource/loader.rs
@@ -15,5 +15,28 @@ pub struct ResourceLoader
 
 impl ResourceLoader
 {
+    // Create empty resource loader
+    fn new() -> Self
+    {
+        ResourceLoader {}
+    }
+    // Add an overall "oncomplete" function to the loader
+    fn with_oncomplete(self) -> Self
+    {
 
+    }
+    // Add an overall "onprogress" function to the loader
+    fn with_onprogress(self) -> Self
+    {
+
+    }
+    // Add a resource to be loaded
+    fn with_resource(self) -> Self
+    {
+        self
+    }
+    fn submit(&mut self)
+    {
+
+    }
 }
