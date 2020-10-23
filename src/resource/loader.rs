09a1f6ed1d4ba361696fcf6bc40bc9d4ab52931a
Earl Kennedy <mnenmenth@gmail.com>
2020-10-22 23:35:33 -0400

    Add some in-progress function bodies
    Submit & drop still needs to be re-written, and the cumulative onloadend and onprogress functions are still WIP

--- src/resource/loader.rs
+++ src/resource/loader.rs
@@ -5,39 +5,131 @@ use wasm_bindgen::
 };
 use web_sys::
 {
-    FileReader,
+    XmlHttpRequest,
+    ProgressEvent,
 };
+use std::
+{
+    cell::{Cell},
+    rc::Rc,
+};
+
+type ProgressClosure = Closure<dyn FnMut(ProgressEvent)>;
+struct HttpRequest
+{
+    // Handle
+    http_request: XmlHttpRequest,
+    // Events
+    onabort: Option<ProgressClosure>,
+    onerror: Option<ProgressClosure>,
+    onload: Option<ProgressClosure>,
+    onloadstart: Option<ProgressClosure>,
+    onloadend: Option<ProgressClosure>,
+    onprogress: Option<ProgressClosure>,
+}
 
 pub struct ResourceLoader
 {
-    cb: Option<Closure<dyn FnMut(JsValue)>>
+    cb: Option<Closure<dyn FnMut(JsValue)>>,
+
+    requests_left: Rc<Cell<i32>>,
+    oncomplete: Option<Closure<dyn FnMut()>>,
+    onprogress: Option<Closure<dyn FnMut(i32, i32)>>,
+    http_requests: Vec<HttpRequest>,
 }
 
 impl ResourceLoader
 {
-    // Create empty resource loader
+    /// Create empty resource loader
     pub fn new() -> Self
     {
         ResourceLoader
         {
-            cb: None
+            cb: None,
+            requests_left: Rc::new(Cell::new(0)),
+            oncomplete: None,
+            onprogress: None,
+            http_requests: vec![],
         }
     }
-    // Add an overall "oncomplete" function to the loader
-    pub fn with_oncomplete(self) -> Self
+    /// Add an overall "when everything is done" function to the loader
+    pub fn set_oncomplete<F>(&mut self, mut callback: F) where F: 'static + FnMut()
     {
-        self
+        self.oncomplete = Some(Closure::wrap(Box::new(callback) as Box<dyn FnMut()>));
     }
-    // Add an overall "onprogress" function to the loader
-    pub fn with_onprogress(self) -> Self
+    /// Add an overall "onprogress" function to the loader
+    ///
+    /// First arg is amount of work already performed
+    /// Second arg is total amount of work to be done
+    pub fn set_onprogress<F>(&mut self, mut callback: F) where F: 'static + FnMut(i32, i32)
     {
-        self
+        self.onprogress = Some(Closure::wrap(Box::new(callback) as Box<dyn FnMut(_, _)>));
     }
-    // Add a resource to be loaded
-    pub fn with_resource(self) -> Self
+    /// Add a resource to be loaded
+    pub fn add_resource<F>(&mut self,
+                           resource_url: &str,
+                           mut onabort: Option<F>,
+                           mut onerror: Option<F>,
+                           mut onload: Option<F>,
+                           mut onloadstart: Option<F>,
+                           mut onloadend: Option<F>,
+                           mut onprogress: Option<F>
+    ) -> Result<(), JsValue> where F: 'static + FnMut(ProgressEvent)
     {
-        self
+        let left = self.requests_left.get();
+        self.requests_left.set(left + 1);
+        let mut request = HttpRequest
+        {
+            http_request: XmlHttpRequest::new()?,
+            onabort: None, onerror: None,
+            onload: None, onloadstart: None, onloadend: None,
+            onprogress: None,
+        };
+        if let Some(onabort) = onabort
+        {
+            let closure = Closure::wrap(Box::new(onabort) as Box<dyn FnMut(_)>);
+            request.onabort = Some(closure);
+        }
+        if let Some(onerror) = onerror
+        {
+            let closure = Closure::wrap(Box::new(onerror) as Box<dyn FnMut(_)>);
+            request.onerror = Some(closure);
+        }
+        if let Some(onload) = onerror
+        {
+            let closure = Closure::wrap(Box::new(onload) as Box<dyn FnMut(_)>);
+            request.onload = Some(closure);
+        }
+        if let Some(onloadstart) = onloadstart
+        {
+            let closure = Closure::wrap(Box::new(onloadstart) as Box<dyn FnMut(_)>);
+            request.onloadstart = Some(closure);
+        }
+        if let Some(onloadend) = onloadend
+        {
+            let request_left = self.requests_left.clone();
+            let closure = Closure::wrap(Box::new(move |event: ProgressEvent|
+                {
+                    let left = self.requests_left.get();
+                    if left <= 1
+                    {
+                        // TODO: cleanup/drop all memory here
+                    }
+                    else
+                    {
+                        self.requests_left.set(left - 1);
+                        onloadend(event);
+                    }
+                }) as Box<dyn FnMut(_)>);
+        }
+        if let Some(onprogress) = onprogress
+        {
+
+        }
+
+        Ok(())
     }
+
     pub fn submit(self)
     {
         let rl = std::rc::Rc::new(std::cell::RefCell::new(self));
