6654f6c4f26dd9c088987d245bf171693c259513
Earl Kennedy <mnenmenth@gmail.com>
2021-02-09 23:22:34 -0500

    Add async_loader file

---
+++ src/resource/async_loader.rs
@@ -0,0 +1,19 @@
+use wasm_bindgen::
+{
+    prelude::*,
+    JsCast,
+};
+use wasm_bindgen_futures::JsFuture;
+
+pub struct AsyncResourceLoader
+{
+
+}
+
+impl AsyncResourceLoader
+{
+    pub fn load(resource: &str) -> Result<>
+    {
+
+    }
+}
--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -1,2 +1,3 @@
 pub mod manager;
 pub mod loader;
+mod async_loader;
