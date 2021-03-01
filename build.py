a2dd9a4095f266af1c3288682399991ba63ea8ba
Earl Kennedy <mnenmenth@gmail.com>
2021-03-01 15:31:10 -0500

    Add web target option. Web target is now default
    This adds an optional flag to switch between the web build target and no-modules build target.
    
    This also makes the web build target the default build target instead of no-modules

--- build.py
+++ build.py
@@ -2,8 +2,6 @@ import os
 import argparse
 import rjsmin
 
-BUILD_REL = "wasm-pack build --out-dir build --no-typescript --target no-modules"
-BUILD_DEV = BUILD_REL + " --dev -- --features \"debug\""
 
 JS_PATH_IN = "./build/swarm_website_visualization.js"
 JS_PATH_OUT = "./build/swarm_website_visualization.min.js"
@@ -11,13 +9,25 @@ JS_PATH_OUT = "./build/swarm_website_visualization.min.js"
 parser = argparse.ArgumentParser(description="Project build script")
 parser.add_argument("-r", "--release", help="Create a release build. Dev/debug build is assumed otherwise", action="store_true")
 parser.add_argument("--no-minjs", help="Disable the creation of a *.min.js", action="store_true")
-args = parser.parse_args();
+parser.add_argument("--no-modules", help="Use the 'no-modules' build target instead of 'web'", action="store_true")
+args = parser.parse_args()
+
+BUILD_REL = "wasm-pack build --out-dir build --no-typescript --target "
+
+if args.no_modules:
+    BUILD_REL += "no-modules"
+else:
+    BUILD_REL += "web"
+
+BUILD_DEV = BUILD_REL + " --dev -- --features \"debug\""
+
 
 if args.release:
     print("Building release build...")
     os.system(BUILD_REL)
 else:
     print("Building dev build...")
+    print(BUILD_DEV)
     os.system(BUILD_DEV)
 print("Done")
 
